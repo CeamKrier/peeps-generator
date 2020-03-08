1f8e36dd985728031bf3d41ea7390b9304edb951
Emre Çakır <ceamkrier@gmail.com>
2020-03-08 22:55:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Open Peeps Generator
+Pick and mix the hand-drawn illustrations at ease!
+
+## Features
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+Thanks :raised_hands:
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/PeepsGenerator.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Peep, {
+	Accessories,
+	Body,
+	Face,
+	FacialHair,
+	Hair,
+	CirclePeep
+} from 'react-peeps'
+
+const styles = {
+  peepStyle: {
+    width: 300,
+    height: 300,
+    justifyContent: 'center',
+    alignSelf: 'center'
+  },
+  circleStyle: {
+    backgroundColor: '#F3D34A',
+    width: 270,
+    height: 270,
+    alignSelf: 'center',
+    borderRadius: 135,
+    overflow: 'hidden',
+    borderWidth: 3,
+    borderColor: 'black',
+    borderStyle: 'solid'
+  },
+  showcaseWrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    height: '-webkit-fill-available'
+  }
+}
+
+const PeepsGenerator: React.FC = () => {
+
+  return (
+    <div style={styles.showcaseWrapper}>
+      <Peep
+        style={styles.peepStyle}
+        accessory={Accessories.GlassRoundThick}
+        body={Body.PointingUp}
+        face={Face.Cute}
+        hair={Hair.Long}
+      />
+
+      <CirclePeep
+        style={styles.peepStyle}
+        accessory={Accessories.GlassRoundThick}
+        body={Body.PointingUp}
+        face={Face.Cute}
+        hair={Hair.Long}
+        circleStyle={styles.circleStyle}
+        viewBox={{x: '0', y: '0', width: '1050', height: '1200'}}
+      />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <PeepsGenerator />,
+  document.getElementById('main')
+)
