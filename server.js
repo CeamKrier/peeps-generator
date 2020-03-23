9b18bc730234082972f543d7799c86c8748fcca7
Emre Çakır <ceamkrier@gmail.com>
2020-03-23 16:16:37 +0300

    feat: gzip text compression implemented to server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression')
 const server = express();
 const port = process.env.PORT || 8080;
 
+server.use(compression())
 server.use(express.static(path.join(__dirname, 'build')));
 
 server.get('*', (req, res) => {
