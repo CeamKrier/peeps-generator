b04ae58d1b0edfde0743c4cdf37b4346f1e3f6f8
Emre Çakır <ceamkrier@gmail.com>
2020-04-16 17:12:08 +0300

    feat: gradient color picker implementation finalized

--- src/components/colorWheel.tsx
+++ src/components/colorWheel.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useProvider } from '../utils/contextProvider';
-import GradientColorPicker from './gradientColorPicker';
+import { ColorResult, ChromePicker } from 'react-color';
 
 interface ColorWheelType {
 	color: string;
 	target: 'first' | 'second';
 }
 
-export const ColorWheel: React.FC<ColorWheelType> = ({ color, target  }) => {
+export const ColorWheel: React.FC<ColorWheelType> = ({ color, target }) => {
 	const { dispatch } = useProvider();
 
+	useEffect(() => {
+		document.querySelectorAll('.flexbox-fix')[1].remove();
+	}, []);
+
 	return (
-		//@ts-ignore
-		<GradientColorPicker color={color} onChange={(pickedColor) => {
-			const requestType =
-				target === 'first' ? 'SET_FIRST_COLOR' : 'SET_SECOND_COLOR';
-			dispatch({
-				type: requestType,
-				payload: pickedColor.hex,
-			});
-		}} />
+		<ChromePicker
+			disableAlpha
+			color={color}
+			onChange={(pickedColor: ColorResult) => {
+				const requestType =
+					target === 'first' ? 'SET_FIRST_COLOR' : 'SET_SECOND_COLOR';
+				dispatch({
+					type: requestType,
+					payload: pickedColor.hex,
+				});
+			}}
+		/>
 	);
 };
-
--- src/components/gradientBuilder.tsx
+++ src/components/gradientBuilder.tsx
@@ -86,6 +86,7 @@ export const GradientBuilder = () => {
 			renderHelper.color = secondColor;
 			renderHelper.target = 'second';
 		}
+
 		return (
 			<>
 				{firstColorBoxClicked && (
