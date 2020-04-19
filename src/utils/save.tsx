a1b0d1b7e5ad4f13dcd93d97eff5f2d3c150bcd5
Emre Çakır <ceamkrier@gmail.com>
2020-04-19 16:58:44 +0300

    fix: safari browsers will be able to download png

--- src/utils/save.tsx
+++ src/utils/save.tsx
@@ -1,51 +1,64 @@
 import * as FileSaver from 'file-saver';
 
 export const saveSvg = (svgEl: HTMLElement, name: string) => {
-  svgEl.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-  const svgData = svgEl.outerHTML;
-  const preface = '<?xml version="1.0" standalone="no"?>\r\n';
-  const svgBlob = new Blob([preface, svgData], {
-    type: 'image/svg+xml'
-  });
-  downloadResource(svgBlob, name);
+	svgEl.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+	const svgData = svgEl.outerHTML;
+	const preface = '<?xml version="1.0" standalone="no"?>\r\n';
+	const svgBlob = new Blob([preface, svgData], {
+		type: 'image/svg+xml',
+	});
+	downloadResource(svgBlob, name);
 };
 
 export const savePng = (
-  svgEl: HTMLElement,
-  name: string,
-  scaleVector: number
+	svgEl: HTMLElement,
+	name: string,
+	scaleVector: number
 ) => {
-  svgEl.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-  const svgData = svgEl.outerHTML;
-  const preface = '<?xml version="1.0" standalone="no"?>\r\n';
-  const canvas = document.createElement('canvas')
-  canvas.width = 390 * scaleVector
-  canvas.height = 390 * scaleVector
+	svgEl.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+	const svgData = svgEl.outerHTML;
+	const preface = '<?xml version="1.0" standalone="no"?>\r\n';
+	const canvas = document.createElement('canvas');
+	canvas.width = 390 * scaleVector;
+	canvas.height = 390 * scaleVector;
 
-  const canvasContext = canvas.getContext('2d');
+	const canvasContext = canvas.getContext('2d');
 
-  const DOMURL = window.self.URL || window.self.webkitURL || window.self;
-  const image = new Image();
-  const svgBlob = new Blob([preface, svgData], {
-    type: 'image/svg+xml'
-  });
-  const url = DOMURL.createObjectURL(svgBlob);
+	const DOMURL = window.self.URL || window.self.webkitURL || window.self;
+	const image = new Image();
+	const svgBlob = new Blob([preface, svgData], {
+		type: 'image/svg+xml',
+	});
+	const url = DOMURL.createObjectURL(svgBlob);
 
-  canvas.style.display = 'none';
-  document.body.appendChild(canvas)
+	canvas.style.display = 'none';
+	document.body.appendChild(canvas);
 
-  image.onload = () => {
-    canvasContext && canvasContext.drawImage(image, 0, 0);
-    DOMURL.revokeObjectURL(url);
-    canvas.toBlob(pngBlob => {
-      pngBlob && downloadResource(pngBlob, name);
-      document.body.removeChild(canvas)
-    });
-  };
-  
-  image.src = url;
+	image.onload = () => {
+		if (!canvasContext) {
+			return;
+		}
+
+		const userAgent = window.navigator.userAgent;
+		if (
+			userAgent.indexOf('Safari') !== -1 &&
+			userAgent.indexOf('Chrome') === -1
+		) {
+			canvasContext.drawImage(image, 0, 0, canvas.width, canvas.height);
+		} else {
+			canvasContext.drawImage(image, 0, 0);
+		}
+
+		DOMURL.revokeObjectURL(url);
+		canvas.toBlob((pngBlob) => {
+			pngBlob && downloadResource(pngBlob, name);
+			document.body.removeChild(canvas);
+		});
+	};
+
+	image.src = url;
 };
 
 const downloadResource = (resource: Blob, name: string) => {
-  FileSaver.saveAs(resource, name);
+	FileSaver.saveAs(resource, name);
 };
