32cfc89117d58ac2a9655d5fe37dfa637d010fe9
ceamkrier <ceamkrier@gmail.com>
2025-09-27 15:05:12 +0300

    Refactor code structure for improved readability and maintainability

--- server/config/index.js
+++
@@ -1,6 +0,0 @@
-const Config = {
-	HTML_FILE_PATH: './build/index.html',
-	BUILD_FOLDER_PATH: './build',
-};
-
-export default Config;
--- server/routes/home.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { serveApp } from '../controllers/home';
-
-const router = express.Router();
-
-router.get('*', serveApp);
-
-export default router;
